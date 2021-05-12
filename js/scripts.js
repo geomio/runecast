e34df87d55032f28d7502b8940fb27a7d378ddb8
geomio <geoff.millner22@gmail.com>
2021-05-12 09:43:09 -0700

    it will loop and show proper runes via js, also code cleaned

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,6 @@ $(document).ready(function(){
     let color1 = 0;
     let day1 = 0;
     let animal1 = 0;
-
-  
     $("input:checkbox[name=time]:checked").each(function(){
       let time = $(this).val();
       time1 += parseInt(time);
@@ -30,17 +28,14 @@ $(document).ready(function(){
     });
 
     let rolls = time1  + color1 + day1 + animal1;
-    console.log(rolls);
 
     let runes = ['fehu', "fehuReverse", 'uruz', "uruzReverse", 'thurisaz', "thurisazReverse", 'ansuz', "ansuzReverse", 'raidho', "raidhoReverse", 'kenaz', "kenazReverse", 'gebo', 'wunjo', "wunjoReverse", 'hagalaz', 'nauthiz', "nauthizReverse", 'isa', 'jera', 'eihwaz', "eihwazReverse", 'perthro', "perthroReverse", 'algiz', "algizReversed", 'sowilo', 'tiwaz', "tiwazReverse", 'berkano', "berkanoReversed", 'ehwaz', "ehwazReverse", 'mannaz', "mannazReverse", 'laguz', "laguzReverse", 'ingwaz', 'othala', "othalaReverse", 'dagaz']
     for (let index = 1; index <= rolls; index++) {
+      let yourRune = (runes[Math.floor(Math.random() * runes.length)]);
+      console.log(yourRune);
       if (index === rolls) {
-        //checks conditions of parentheses
+        $(`#${yourRune}`).show();
       }
-    }
-
-    // let yourRune = (runes[Math.floor(Math.random() * runes.length)]);
-
-    // $(`#${yourRune}`).show();
+    } 
   });
 });
